195dd87052d137cc489509640bcf90e325f27b9f
dafafa19871115-cmd <dafafa19871115@gmail.com>
2026-07-20 23:16:59 +0700

    app.js

--- app.js
+++ app.js
@@ -1,23 +1,246 @@
-// 產生訂單編號
+// 飲料資料
+
+const products=[
+
+{
+id:1,
+name:"綠豆沙",
+price:60
+},
+
+{
+id:2,
+name:"葡萄冰沙",
+price:70
+},
+
+{
+id:3,
+name:"芒果冰沙",
+price:80
+}
+
+];
+
+
+
+let cart=[];
+
+
+
+// 顯示菜單
+
+const menu =
+document.getElementById("menu");
+
+
+products.forEach(item=>{
+
+
+menu.innerHTML +=`
+
+<div class="drink">
+
+
+<h3>
+${item.name}
+</h3>
+
+
+<p>
+${item.price} 元
+</p>
+
+
+<button onclick="addCart(${item.id})">
+加入
+</button>
+
+
+</div>
+
+
+`;
+
+
+});
+
+
+
+
+// 加入購物車
+
+window.addCart=function(id){
+
+
+let item =
+products.find(
+p=>p.id===id
+);
+
+
+cart.push(item);
+
+
+showCart();
+
+
+};
+
+
+
+
+// 顯示購物車
+
+function showCart(){
+
+
+let box =
+document.getElementById("cart");
+
+
+box.innerHTML="";
+
+
+let total=0;
+
+
+
+cart.forEach(item=>{
+
+
+box.innerHTML +=`
+
+<p>
+${item.name}
+ ${item.price} 元
+</p>
+
+`;
+
+
+total+=item.price;
+
+
+});
+
+
+document.getElementById("total")
+.innerHTML=
+"總金額："+total+" 元";
+
+
+}
+
+
+
+
+// 自動產生訂單編號
+
 function createOrderNumber(){
 
-    let number =
-    localStorage.getItem("orderNumber") || 0;
+
+let number =
+localStorage.getItem(
+"orderNumber"
+) || 0;
 
 
-    number++;
+
+number++;
 
 
-    localStorage.setItem(
-        "orderNumber",
-        number
-    );
+
+localStorage.setItem(
+"orderNumber",
+number
+);
 
 
-    let orderId =
-    "A" + String(number).padStart(5,"0");
+
+return "A"
++
+String(number)
+.padStart(5,"0");
 
 
-    return orderId;
+}
+
+
+
+
+//送出訂單
+
+window.submitOrder=function(){
+
+
+if(cart.length===0){
+
+
+alert(
+"請先選擇飲料"
+);
+
+
+return;
+
+
+}
+
+
+
+//產生編號
+
+let orderId =
+createOrderNumber();
+
+
+
+
+//建立訂單
+
+let order={
+
+
+orderId:orderId,
+
+
+items:cart,
+
+
+status:"待製作",
+
+
+time:
+new Date()
+.toLocaleString()
+
+
+};
+
+
+
+
+//目前先顯示
+
+console.log(order);
+
+
+
+alert(
+
+"訂單成立\n\n"+
+"訂單編號："+
+orderId
+
+);
+
+
+
+cart=[];
+
+
+showCart();
+
 
 }
