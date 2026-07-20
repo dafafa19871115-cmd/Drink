b5ac58c709604995e1886812cae0ab482c5fefa5
dafafa19871115-cmd <dafafa19871115@gmail.com>
2026-07-21 02:51:34 +0700

    admin.js

--- admin.js
+++ admin.js
@@ -1,6 +1,6 @@
 const API_URL =
 
-"https://script.google.com/macros/s/AKfycbxo3Gda0zUuI8gkaHSqjJza5KaEn0CILxfjdONDkpjoIoE2cLGEojEwbTuX1kQfh3FKwg/exec";
+"https://script.google.com/macros/s/AKfycby-mrlZ-yU1xs8OXXQK0PoDEqMg3_O1RGTRUKG4syTlk9BajybMw7pcZF7V5GT9Awj-SA/exec";
 
 
 
