2174ee99b46ae87c72ee1ea215cc4d92279852ba
dafafa19871115-cmd <dafafa19871115@gmail.com>
2026-07-21 07:47:52 +0700

    app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ console.log("Bing Sha app.js 正確載入");
 
 
 const API_URL =
-"https://script.google.com/macros/s/AKfycbzYbRBFX8Mjp7GZOReL5i3iDHZvAYReL6b4cIoOTsyJGmyxKou5btNowuW6fWCx6BKQ3A/exec";
+"https://script.google.com/macros/s/AKfycbw98AF0NlYSf6tS0fs1h7_FSJjMhvFty5jSCif4cyHWOGtmADxRKj0Coe92nPOsDT_j9w/exec";
 
 
 
